d5f8350b9e50f9bb59c8e68aba09f2552f9e3b38
Alena <al.gnevusheva@gmail.com>
2025-05-31 20:45:29 +0200

    fix fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${roboto.variable} ${roboto.variable} ${styles.app}`}>
+      <body className={`${roboto.className} ${styles.app}`}>
         <main>
           <MainBackground>{children}</MainBackground>
         </main>
